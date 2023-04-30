031f317b88d3820795df07dc1ec69681209f85c7
Lizaveta Petrova macOS <goldglaider@gmail.com>
2023-04-30 13:26:20 +0300

    feat: add pagination

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,14 @@
+import {useState} from 'react';
+
 import {Filters, SearchInput, VacancyItem} from '@components';
-import {Flex} from '@mantine/core';
+import {Flex, Pagination} from '@mantine/core';
 import {responsiveWidth} from '@utils';
 
 const HEADER_HEIGHT = 84;
 const DESIGN_EXAMPLE_WINDOW_HEIGHT = 900;
 
 const HomePage = () => {
+  const [activePage, setPage] = useState(1);
   return (
     <Flex
       justify="center"
@@ -17,12 +20,27 @@ const HomePage = () => {
       <Flex>
         <Filters />
       </Flex>
-      <Flex direction="column" gap={16}>
-        <SearchInput />
-        <VacancyItem />
-        <VacancyItem />
-        <VacancyItem />
-        <VacancyItem />
+      <Flex direction="column" justify="center" align="center" gap={40}>
+        <Flex direction="column" gap={16}>
+          <SearchInput />
+          <VacancyItem />
+          <VacancyItem />
+          <VacancyItem />
+          <VacancyItem />
+        </Flex>
+        <Pagination
+          total={3}
+          value={activePage}
+          onChange={setPage}
+          color="blue.4"
+          styles={(theme) => ({
+            control: {
+              '&[data-active]': {
+                backgroundImage: theme.colors.blue[2],
+              },
+            },
+          })}
+        />
       </Flex>
     </Flex>
   );
